cc5c766f454f7aa51bc8ef3bcf12c9652d66ad8b
tom <tom@that-place>
2019-05-03 10:56:08 -0500

    Migrating to clj 1.10

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]                 
                  [spork "0.2.1.1-SNAPSHOT"]
                  ;;used to generate ns keys for spec...
                  ;;will likely shift to metosin spec-tools
                  ;;in very near future to obviate this.
                  [irresponsible/spectra "0.2.1"]
+                 [metosin/spec-tools "0.9.2-alpha1"]
                  ;;maintaining backwards compat with 1.8
                  ;;will likely migrate in near future.
-                 [clojure-future-spec "1.9.0-beta4"]
-                 [org.clojure/test.check "0.9.0"]])
+                 #_[clojure-future-spec "1.9.0-beta4"]
+                 [org.clojure/test.check "0.10.0-alpha4"]])
