442785ff894184caea8b698709e23e51e4fc04ef
tom <tom@the-cloud>
2021-04-08 16:44:10 +0000

    Version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject marathon-schemas "4.1.6-SNAPSHOT"
+(defproject marathon-schemas "4.1.7-SNAPSHOT"
   :description "data schemas, specifications, and validation tools for marathon"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]                 
-                 [spork "0.2.1.1-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [spork "0.2.1.4-SNAPSHOT"]
                  ;;used to generate ns keys for spec...
                  ;;will likely shift to metosin spec-tools
                  ;;in very near future to obviate this.
